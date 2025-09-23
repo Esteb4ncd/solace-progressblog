502a92b732a08810d186fc5f5bbe950a441f7268
Esteb4ncd <davidecruzd10@gmail.com>
2025-09-22 23:23:23 -0700

    changing object and post component to be able to do innerHTML + post week 2

--- src/components/PostPage.js
+++ src/components/PostPage.js
@@ -1,7 +1,28 @@
+// import React from "react";
+// import { useParams, Link } from "react-router-dom";
+// import posts from "../posts";
+// // import "./PostPage.css";
+
+// export default function PostPage() {
+//   const { id } = useParams();
+//   const post = posts.find((p) => p.id === parseInt(id));
+
+//   if (!post) return <p>Post not found</p>;
+
+//   return (
+//     <div className="post-page">
+//       <Link to="/">← Back to All Posts</Link>
+//       <h1>{post.header}</h1>
+//       <p>{post.date}</p>
+//       <img src={post.image} alt={post.header} />
+//       <p>{post.description}</p>
+//     </div>
+//   );
+// }
+
 import React from "react";
 import { useParams, Link } from "react-router-dom";
 import posts from "../posts";
-// import "./PostPage.css";
 
 export default function PostPage() {
   const { id } = useParams();
@@ -15,8 +36,12 @@ export default function PostPage() {
       <h1>{post.header}</h1>
       <p>{post.date}</p>
       <img src={post.image} alt={post.header} />
-      <p>{post.description}</p>
+
+      {/* render description as HTML */}
+      <div
+        className="prose mt-4"
+        dangerouslySetInnerHTML={{ __html: post.description }}
+      />
     </div>
   );
 }
-
